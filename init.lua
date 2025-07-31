cd7deb8e8787aaa4bd2ac2d5ce1aec4ed2693d42
Marcel Bonkowski <marcel.bonkowski@web.de>
2025-07-31 22:37:11 +0200

    feat: add noice

--- init.lua
+++ init.lua
@@ -84,6 +84,9 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+-- Suppress deprecated warnings from plugins
+vim.deprecate = function() end
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
